f6f1d3ebac6935385f5e07f781f6f0cd1ad9a94d
zhaofeng-shu33 <616545598@qq.com>
2018-08-15 00:02:30 +0800

    modify init

--- ace_cream/__init__.py
+++ ace_cream/__init__.py
@@ -2,10 +2,15 @@ import os
 from distutils.sysconfig import get_python_lib
 # For gfortran+msvc combination, extra shared libraries may exist (stored by numpy.distutils)
 if os.name == 'nt':
+    HAS_DLL = False
     extra_dll_dir = os.path.join(get_python_lib(), 'ace_cream', '.libs')
     if os.path.isdir(extra_dll_dir):
+        extra_dll_dir = os.path.join(get_python_lib(), 'ace_internal', '.libs')
+        if os.path.isdir(extra_dll_dir):
+            HAS_DLL = True
+    if(HAS_DLL):
         os.environ['PATH'] += os.pathsep + extra_dll_dir
     else:
-        raise ImportError('ace_cream/.libs not found')
+        raise ImportError('directory %s ace_cream/.libs or %s ace_internal/.libs not found'%(get_python_lib(),get_python_lib()))
 from .ace_cream import ace_cream
 from .ace_cream import f_mapping

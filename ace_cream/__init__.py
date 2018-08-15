52b1ade7199ac8d2f5b172c41e1713ef9824fa99
zhaofeng-shu33 <616545598@qq.com>
2018-08-15 23:00:55 +0800

    add conda build support for windows

--- ace_cream/__init__.py
+++ ace_cream/__init__.py
@@ -13,6 +13,9 @@ if os.name == 'nt':
     if(HAS_DLL):
         os.environ['PATH'] += os.pathsep + extra_dll_dir
     else:
-        raise ImportError('directory %s/ace_cream/.libs or %s/ace_internal/.libs not found'%(get_python_lib(),get_python_lib()))
+        if(os.path.exists(os.path.join(get_python_lib(), 'ace_internal.pyd'))):
+            os.environ['PATH'] += get_python_lib()
+        else:
+            raise ImportError('directory %s/ace_cream/.libs or %s/ace_internal/.libs not found'%(get_python_lib(),get_python_lib()))
 from .ace_cream import ace_cream
 from .ace_cream import f_mapping
